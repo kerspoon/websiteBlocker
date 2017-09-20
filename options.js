a295055fa9ba3914ccd4b3b21257909fa7be3138
James Brooks <kerspoon@gmail.com>
2017-09-20 19:53:00 +0100

    bugfix: allow saving debugMode state

--- options.js
+++ options.js
@@ -1,17 +1,26 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
-  console.log('save_options');
 
   var basicPatterns = document.getElementById('basicPatterns').value;
   var fullPatterns = document.getElementById('fullPatterns').value;
   var timeoutPatterns = document.getElementById('timeoutPatterns').value;
   var frequency = document.getElementById('frequency').value;
+  var debugMode = document.getElementById('debugMode').checked;
+
+  console.log("save_options", {
+    basicPatterns: basicPatterns,
+    fullPatterns: fullPatterns,
+    timeoutPatterns: timeoutPatterns,
+    frequency: frequency,
+    debugMode: '' + debugMode
+  });
 
   chrome.storage.sync.set({
     basicPatterns: basicPatterns,
     fullPatterns: fullPatterns,
     timeoutPatterns: timeoutPatterns,
-    frequency: frequency
+    frequency: frequency,
+    debugMode: debugMode
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -25,17 +34,19 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  console.log('restore_options');
   chrome.storage.sync.get({
     basicPatterns: 'dailymail\\.co\nexpress\\.co',
     fullPatterns: '',
     timeoutPatterns: '',
-    frequency: '5'
+    frequency: '5',
+    debugMode: 'false'
   }, function(items) {
+    console.log("restore_options", items);
     document.getElementById('basicPatterns').value = items.basicPatterns;
     document.getElementById('fullPatterns').value = items.fullPatterns;
     document.getElementById('timeoutPatterns').value = items.timeoutPatterns;
     document.getElementById('frequency').value = items.frequency;
+    document.getElementById('debugMode').checked = items.debugMode;
   });
 }
 
